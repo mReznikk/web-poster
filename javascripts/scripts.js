5eb81187a41e617fc2ea0e8199de7cf7ec3b1dcc
MariaHSE <cyberzia3000@gmail.com>
2026-03-29 12:31:41 +0300

    running line done

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,6 +1,6 @@
 const cctv = document.getElementById('screen-cctv');
 cctv.addEventListener('click', () => {
-    let randomNumber = Math.floor(Math.random() * 3) + 1;
+    let randomNumber = Math.floor(Math.random() * 4) + 1;
     console.log(randomNumber);
     cctv.className = `screen-cctv-base screen-cctv-image-${randomNumber}`;
 });
